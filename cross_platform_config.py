868168527321367a44c00909ac5c38fa24259eea
manpeihong <pman3@uic.edu>
2018-01-26 10:59:18 -0600

    Modify UI size.

--- cross_platform_config.py
+++ cross_platform_config.py
@@ -4,7 +4,7 @@ __version__ = '0.01'
 
 
 class config:
-    if _platform == "darwin" or _platform == "linux" or _platform == "linux2":
+    if _platform == "darwin":
         # MAC OS X or linux
         FRAME_WIDTH = 1000
         FRAME_HEIGHT = 650
@@ -12,10 +12,10 @@ class config:
         ENTRY_WIDTH = 8
         OPTIONMENU_WIDTH = 38
 
-    elif _platform == "win32" or _platform == "win64":
+    elif _platform == "win32" or _platform == "win64" or _platform == "linux" or _platform == "linux2":
         # Windows
         FRAME_WIDTH = 1150
-        FRAME_HEIGHT = 920
+        FRAME_HEIGHT = 700
         LABEL_WIDTH = 12
         ENTRY_WIDTH = 8
         OPTIONMENU_WIDTH = 34
